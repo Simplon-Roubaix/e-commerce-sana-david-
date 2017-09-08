fd595106713d172d68e27ae31531d3ded2cccee1
Ousalem.david@netc.fr <you@example.com>
2017-09-08 11:27:57 +0200

    Insertion footer final

--- information_footer.php
+++ information_footer.php
@@ -3,3 +3,6 @@
 
 $informationF = array ('Mentions legales ','Chartes de confidentialite',' Plan du site',' Copyright © 2048  Sanouo & Ousalem');
      ?>
+
+<?php
+$informationH = array ('Titre' , 'le site numéro 1 !'); ?>
