0391901b0092baff16499af63c263aacaef6eb06
sanouo <sanouo591@gmail.com>
2017-09-11 15:11:23 +0200

    css file product

--- detail.php
+++ detail.php
@@ -4,22 +4,23 @@
 $article = $_GET['article'];
 ?>
 
-<div class="retour">
-  <a href="index.php" class="btn btn-primary tonbou">Accueil</a>
-</div>
+<!-- <div class="retour">
+  <a href="index.php" class="btn btn-primary tonbou">Retour</a>
+</div> -->
 
 
 
 
 
 
-<div class="tecar">
-  <img src="<?php echo $coordonnees[$article]['image'];  ?>" alt="moto">
+<div class="tecar card col-8 col-md-8 col-lg-8">
+  <img src="<?php echo $coordonnees[$article]['image'];  ?>" class="img-fluid" alt="moto">
   <div class="inf">
     <h4 class="name"><?php echo $coordonnees[$article]['titre'];  ?></h4>
     <p>
       <?php echo $coordonnees[$article]['descriptionMax'];  ?>
     </p>
+    <a href="index.php" class="btn btn-primary tonbou">Retour</a>
     <a href="#" class="btn btn-primary tonbou">Achetez</a>
   </div>
 </div>
--- section.php
+++ section.php
@@ -6,25 +6,103 @@ $coordonnees = array(
 1=>  [ 'image' => './img/moto1.jpg',
     'titre' => 'Kawasaki',
   'description' => 'ZX-10R',
-  'descriptionMax' => 'ezhfjkjkjdkfjkjfkjfkjfkfsjsk'
+  'descriptionMax' => "La ZX-10R est un modèle de motocyclette du constructeur japonais Kawasaki.
+La ZX-10R est apparue en 2004 en remplacement de la ZX-9R pour permettre à Kawasaki de rester compétitif face aux autres marques qui proposaient déjà des quatre cylindres de 1000 cm³.
+Le moteur avec l'alimentation forcée en air, appelée Ram Air, développe 184 ch à 11 700 tr/min.
+Les étriers de frein avant ont une fixation radiale.
+Elle est disponible en vert/noir, bleu/noir, gris/noir ou noir.
+Depuis 2004, plusieurs générations se sont succédé :
+    en 2006, modification du châssis (bras oscillant notamment), évolution du moteur (184 chevaux), 2 sorties d'échappement sous la selle, esthétique. Coloris Vert/Noir, Noir, Gris/Noir.
+    en 2008, nouveau châssis, le moteur gagne encore quelques chevaux (188 chevaux), retour de la sortie d'échappement en position classique, coloris : Vert/Noir, Noir en 2008, Blanc/Noir en 2009, Orange/Noir et Bleu/Noir selon les pays.
+Les disques de frein avant et arrière passent respectivement à 320 et 220 mm. L'empattement gagne 5 mm et le poids 5 kg. Le moteur développe 188 ch à 12 500 tr/min, ou 200 ch avec Ram Air, pour un couple de 11,9 mkg à 8 700 tr/min.
+    en 2011, évolution importante avec notamment : nouveau châssis (le cadre devient périmétrique, nouvelle fourche BPF - Big Piston Fork, amortisseur arrière en position horizontale), nouvelle esthétique, moteur développant 200 chevaux, contrôle de traction et 3 modes de puissance réglables. Coloris Vert/Noir, Noir.
+En 2004 et 2005 la ZX10R a été vainqueur du Masterbike (en), épreuve pour déterminer la meilleure motocyclette sportive de production de l'année.
+En 2006, 2008 et 2009, elle est élue moto sportive de l'année lors d'un maxi-test européen <br> Prix : 18000$"
   ],
 
 2=>   [ 'image' => './img/moto2.jpg',
     'titre' => 'Moto Guzzi',
   'description' => 'V9 Bobber',
-'descriptionMax' => 'ezhfjkjkjdkfjkjfkjfkjfkfsjsk'
+'descriptionMax' => 'Fiche Technique : Moteur	bicylindre 4T, refroidissement par air et huile, double ACT, 2 soupapes
+Cylindrée	853 cm3
+Alésage x Course	84 x 77 mm mm
+Puissance	40,44 kW kW à 6.250 tr/min
+Couple maxi	62 Nm Nm à 3000 tr/min
+Transmission
+Transmission	6 rapports, cardan
+Suspension
+Suspension avant	fourche télescopique, Ø 41 mm
+Suspension arrière	bras oscillant avec double combiné ajustable en précontrainte
+Pneus
+Pneu avant	130/90 x 16
+Pneu arrière	150/80 x 16
+Freinage
+Frein avant	Disque flottant, diamètre 320 mm
+Frein arrière	Disque flottant, diamètre 260 mm
+Divers
+Mise en route	démarreur électrique
+Poids	200 kg
+Hauteur de selle	770 mm mm
+Longueur	2124 mm
+Empattement	1480 mm <br> Prix : 12 000$'
   ],
 
 3=>   [ 'image' => './img/moto3.jpg',
     'titre' => 'KSR MOTO',
   'description' => 'TW 125 SM',
-'descriptionMax' => 'ezhfjkjkjdkfjkjfkjfkjfkfsjsk'
+'descriptionMax' => "Données:
+Type: 4 temps, refroidi par l'eau
+Cylindree: 125 cm³
+Puissance maxi: 8,0 kW @ 9.000 rpm
+Couple maxi: 9,0 Nm @ 7.000 rpm
+Capacité de carburant: 7 l
+Transmission: 5 vitesses
+Masse en service: 130 kg
+Démarrage: electrique
+Freins: à disque/à disque
+Nombre de places: 2
+Longeur: 2.210 mm
+Largeur: 835 mm
+Hauteur: 1.190 mm
+Empattement: 1.440 mm
+Consommation: 1,9 l/100 km
+Emission de CO2: 44 g/km
+Coloris:
+Orange, Noir <br> Prix : 3500$"
   ],
 
 4=>   [ 'image' => './img/moto4.jpg',
     'titre' => 'Tuono V4',
   'description' => '1100 RR',
-'descriptionMax' => 'ezhfjkjkjdkfjkjfkjfkjfkfsjsk'
+'descriptionMax' => 'Fiche technique Aprilia Tuono V4 R
+Type moteur : 4 cylindres en V à 65°, 4 temps, refroidi par eau, double ACT et 4 soupapes par cylindre
+Alesage x course : 78 x 52,3 mm
+cylindrée : 999,6 cm3
+Taux de compression : 13 :1
+Puissance maxi : 162 ch à 11 000 tr/min
+Couple maxi : 11 mkg à 9 000 tr/min
+Alimentation : Par injection Magneti Marelli Ø 48 mm, 3 cartographies au choix T (Track), S (Sport) ou R (Road)
+Allumage : Electronic digital, 1 bougie “crayon”/cylindre. Système APRC incluant Traction Control (ATC), Wheelie Control (AWC) et Launch Control (ALC)
+Mise en route : Démarreur électrique
+Lubrification : Carter humide
+Boîte de vitesses : 6 rapports
+Transmission secondaire : Par chaîne, braquet 42/12 (2,625)
+Cadre : À double longeron, en aluminium
+Suspension avant : Fourche inversée Sachs Ø 43 mm, réglable en précharge de ressorts, en compression et détente hydraulique. Débattement 120 mm
+Suspension arrière : Bras en alu actionnant un combiné unique réglable en précharge de ressorts, en compression et détente hydraulique. Débattement 130 mm
+Freins Avant : double disque Ø 320 mm, étriers 4 pistons à fixation radiale.
+Freins Arrière : disque Ø 220 mm, étrier 2 pistons
+Pneus : 120/70 - 17" AV, 190/55 - 17" AR (avec option possible en 190/50 ou 200/55)
+Dimensions :
+Longueur HT 2 060 mm
+Largeur HT 800 mm
+Hauteur 1,100 mm
+Hauteur de selle 840 mm
+Empattement 1,440 mm
+Angle de colonne : 25°
+Chasse : 107,5 mm
+Contenance réservoir : 17 litres (dont 4 de réserve)
+Poids à sec : 179 kg <br> Prix : 20000$'
   ],
 
 
