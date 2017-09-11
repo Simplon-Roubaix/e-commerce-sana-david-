e8eeb34fcf6f6589d5ee9189b97a0d1e910c32a8
sanouo <sanouo591@gmail.com>
2017-09-11 14:07:05 +0200

    file product

---
+++ detail.php
@@ -0,0 +1,32 @@
+<?php include ("section.php");
+ include ("header.php");
+
+$article = $_GET['article'];
+?>
+
+<div class="retour">
+  <a href="index.php" class="btn btn-primary tonbou">Accueil</a>
+</div>
+
+
+
+
+
+
+<div class="tecar">
+  <img src="<?php echo $coordonnees[$article]['image'];  ?>" alt="moto">
+  <div class="inf">
+    <h4 class="name"><?php echo $coordonnees[$article]['titre'];  ?></h4>
+    <p>
+      <?php echo $coordonnees[$article]['descriptionMax'];  ?>
+    </p>
+    <a href="#" class="btn btn-primary tonbou">Achetez</a>
+  </div>
+</div>
+
+
+
+
+
+
+<?php include("footer.php"); ?>
